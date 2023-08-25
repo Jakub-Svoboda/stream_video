75f77b6e37505483f1d6903e182ef95e296ac2dd
Jakub Svoboda <jakub.svoboda.developer@gmail.com>
2023-08-25 15:24:56 +0200

    Initialize the video_manager app and a basic model for videos

---
+++ video_manager/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Video(models.Model):
+    name = models.CharField(max_length=255)
+    shortName = models.CharField(max_length=255, blank=True)
+    iconUri = models.URLField()
+    manifestUri = models.URLField()
+    source = models.CharField(max_length=255)
+    focus = models.BooleanField(default=False)
+    disabled = models.BooleanField(default=False)
+    extraText = models.JSONField(default=list)
+    certificateUri = models.URLField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    isFeatured = models.BooleanField(default=False)
+    drm = models.JSONField(default=list)
+    features = models.JSONField(default=list)
+    licenseServers = models.JSONField(default=dict)
+    licenseRequestHeaders = models.JSONField(default=dict)
+    requestFilter = models.TextField(null=True, blank=True)
+    responseFilter = models.TextField(null=True, blank=True)
+    clearKeys = models.JSONField(default=dict)
+    extraConfig = models.JSONField(null=True, blank=True)
+    adTagUri = models.URLField(null=True, blank=True)
+    imaVideoId = models.CharField(max_length=255, null=True, blank=True)
+    imaAssetKey = models.CharField(max_length=255, null=True, blank=True)
+    imaContentSrcId = models.CharField(max_length=255, null=True, blank=True)
+    mimeType = models.CharField(max_length=255, null=True, blank=True)
+    mediaPlaylistFullMimeType = models.CharField(max_length=255, null=True, blank=True)
+    storedProgress = models.FloatField(default=1)
+    storedContent = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
